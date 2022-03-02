c539804e6cb4affbc9abf00ff73bf412c677ebf3
Sazzad Hossain <sazzad.csebubt@gmail.com>
2022-03-02 09:58:04 +0600

    first commit

--- script.js
+++ script.js
@@ -13,24 +13,50 @@ function ClickBtn() {
 showallMobile = (items) => {
     const searchResult = document.getElementById('mobilecard');
     searchResult.textContent = '';
-    for (const item of items) {
-        console.log(item);
+    for (const item of items.slice(0, 20)) {
+        // console.log(item);
         const newDiv = document.createElement("div");
         newDiv.classList.add('col-sm-4');
         newDiv.innerHTML = `
      
-        <div class="card mt-4 style="height:30rem">
-            <img src="${item.image}" class="card-img-top" alt="...">
-            <div class="card-body"style="width:400px mb-5">
-            <h5 class="card-title">Brand: ${item.brand}</h5>
+     <div class="card mt-4 style="height:30rem">
+               <img src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body"style="width:400px mb-5">
+                <h5 class="card-title">Brand: ${item.brand}</h5>
                 <h5 class="card-title">Mobile :${item.phone_name}</h5>
                 <p class="card-text">${item.slug}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        
-    </div>
+         </div>
+         <button onclick="loadMealDetails('${item.slug}')"class="btn btn-primary">More Information</button>
+     </div>
 
         `;
+        // console.log(item.slug);
         searchResult.appendChild(newDiv)
     }
+}
+
+const loadMealDetails = id => {
+
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showdetails(data.data))
+}
+
+const showdetails = item => {
+
+    console.log(item);
+    const details = document.getElementById('showdetails');
+    const div = document.createElement('div');
+    details.textContent = '';
+    div.classList.add('card')
+    div.innerHTML = `
+    <img class="card-img-top" src="${item.image}"alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+        </div>
+    `;
+    details.appendChild(div)
 }
